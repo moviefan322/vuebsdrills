49b74a0cb903cbb4ec931a25981bdd0f53a57962
moviefan322 <philman202@yahoo.com>
2024-05-24 20:43:26 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCounterStore } from './stores/counter'
+
+const counter = useCounterStore()
+
+const add = () => {
+  counter.increment()
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ counter.count }}</h1>
+    <h2>{{ counter.doubleCount }}</h2>
+    <button @click="add">++</button>
+  </div>
+</template>
+
+<style scoped></style>
