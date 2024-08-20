39ae8d1d3ca3d034e16b470ff101d878b6f09f8f
moviefan322 <philman202@yahoo.com>
2024-08-20 04:02:06 -0400

    adds home icon to top bar

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="userbar">
-    <p v-if="!isUserLoggedIn">
-      <router-link to="/auth" class="noStyleLink lime">Login</router-link>
-    </p>
-    <p v-else><router-link to="/history" class="noStyleLink lime">Stats</router-link></p>
+    <div>
+      <p>
+        <router-link to="/" class="noStyleLink lime">
+          <img :src="PoolBallSvg" alt="Pool Ball" height="40" width="40"
+        /></router-link>
+      </p>
+    </div>
+    <div>
+      <p v-if="!isUserLoggedIn">
+        <router-link to="/auth" class="noStyleLink lime">Login</router-link>
+      </p>
+      <p v-else><router-link to="/history" class="noStyleLink lime">Stats</router-link></p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useAuthStore } from '../stores/auth'
+import PoolBallSvg from '../assets/poolBall.svg'
 
 const store = useAuthStore()
 
@@ -18,7 +28,9 @@ const isUserLoggedIn = computed(() => !!store.getToken())
 
 <style scoped>
 #userbar {
-  display: inline;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   border-bottom: 1px dotted lime;
 }
